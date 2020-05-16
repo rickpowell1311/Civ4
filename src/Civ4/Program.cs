96828789205d86ae9fedb58b76bad98bdcbfe2fb
Rick <rickpowell1311@gmail.com>
2020-05-16 17:32:28 +0100

    Add src

---
+++ src/Civ4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Civ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Megalomaniacs!");
+        }
+    }
+}
